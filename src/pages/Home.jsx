ddeae1986f945c1275839e2bb75720b935856e6e
Prabhakar <prabhakarpanwar808@gmail.com>
2026-01-28 21:50:39 +0530

    Done by P

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import Card from "../components/Card";
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div>
+      <Card />
+    </div>
+  );
 }
 
-export default Home
+export default Home;
