d2f601ee7671e53dd27b602b0dcd6b8a000a00cd
Nitin0264 <nc8224140@gmail.com>
2026-01-29 01:17:33 +0530

    done by N

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Hero from "../components/Hero";
+import Card from "../components/Card";
 
 function Home() {
   return (
     <div>
       <Hero/>
-      
+      <Card/>
 
     </div>
   );
